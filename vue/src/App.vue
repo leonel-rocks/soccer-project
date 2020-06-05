5843252e6b313e3f5708937428fa4cca7b7f2aaf
Leonel_Ocsa <leonel.ocsa.sanchez@ucsp.edu.pe>
2020-06-05 11:37:27 -0500

    Team component created, it renders players as a table

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Team/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Team from './components/Team.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Team,
   }
 }
 </script>
---
+++ vue/src/components/Team.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Team Component</h1>
+        <md-table v-model="players">
+            <md-table-row slot="md-table-row" slot-scope="{ item }" md-selectable="multiple" md-auto-select>
+                <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+                <md-table-cell md-label="Position" md-sort-by="position">{{ item.position }}</md-table-cell>
+                <md-table-cell md-label="Goals" md-sort-by="goals">{{ item.goals }}</md-table-cell>
+            </md-table-row>
+        </md-table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+
+        },
+        computed: {
+            players() {
+                return [
+                    {name: 'Jugador A', position: 'position A', goals: 0},
+                    {name: 'Jugador B', position: 'position B', goals: 0},
+                    {name: 'Jugador C', position: 'position C', goals: 0},
+                    {name: 'Jugador D', position: 'position D', goals: 0},
+                    {name: 'Jugador E', position: 'position E', goals: 0},
+                    {name: 'Jugador F', position: 'position F', goals: 0},
+                ]
+            }
+        }
+    }
+</script>
