061d33f4062d963c22dd62a58ffd9da7da4b3444
Leonel_Ocsa <leonel.ocsa.sanchez@ucsp.edu.pe>
2020-06-05 12:27:01 -0500

    added checkbox selectable options to each row at players table

--- vue/src/components/Team.vue
+++ vue/src/components/Team.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <h1>Team Component</h1>
-        <md-table v-model="players">
-            <md-table-row slot="md-table-row" slot-scope="{ item }" md-selectable="multiple" md-auto-select>
+        <md-table v-model="players" md-card @md-selected="onSelect">
+            <md-table-row 
+                slot="md-table-row" 
+                slot-scope="{ item }" 
+                md-selectable="multiple" 
+                md-auto-select
+            >
                 <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
                 <md-table-cell md-label="Position" md-sort-by="position">{{ item.position }}</md-table-cell>
                 <md-table-cell md-label="Goals" md-sort-by="goals">{{ item.goals }}</md-table-cell>
@@ -27,6 +32,17 @@
                     {name: 'Jugador F', position: 'position F', goals: 0},
                 ]
             }
+        },
+        methods: {
+            onSelect (items) {
+                this.selected = items
+            },    
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .md-table + .md-table {
+        margin-top: 16px;
+    }
+</style>
