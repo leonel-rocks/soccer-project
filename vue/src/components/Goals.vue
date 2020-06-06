aeb95e6aec493a30cea270876edfdf0cc29c0430
Leonel_Ocsa <leonel.ocsa.sanchez@ucsp.edu.pe>
2020-06-06 00:00:49 -0500

    Goals component added, also increment and decrement events added for scores

---
+++ vue/src/components/Goals.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+       <minus-circle v-on:click="decrementScore"/> {{ goalsScored }} <plus-circle v-on:click="incrementScore"/>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            scored: { type: Number, default: 0} 
+        },
+        data: function (){
+            return {
+                goalsScored: this.scored,
+            }
+        },
+        methods: {
+            decrementScore(event) {
+                this.goalsScored--
+                event.stopPropagation()
+            },
+            incrementScore() {
+                this.goalsScored++
+                event.stopPropagation()
+            }
+        }
+    }
+</script>
